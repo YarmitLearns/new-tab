8277886b5a64bcc0f2040efe3c722df8b1da0420
leap-NP <l34p@tutanota.com>
2017-03-22 00:01:01 -0700

    Set up db migrations

--- manage.py
+++ manage.py
@@ -4,16 +4,18 @@ from app import create_app, db
 # it was for Migrate, something I am not using.
 from app.models import Notes, Prides
 from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
+migrate = Migrate(app, db)
 
 
 def make_shell_context():
 	return dict(app=app, db=db,
 		Notes=Notes, Prides=Prides)
 manager.add_command("shell", Shell(make_context=make_shell_context))
-
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
 	manager.run()
